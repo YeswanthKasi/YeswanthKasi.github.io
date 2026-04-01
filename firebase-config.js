babdd2a1bb252bf06b0b0eb57df51ec2edb8e6e0
School Management Dev <developer@schmng.app>
2026-04-01 20:25:30 +0530

    Update owner UID for admin and Firestore authorization

--- firebase-config.js
+++ firebase-config.js
@@ -7,7 +7,7 @@ window.KASIREDDI_FIREBASE_CONFIG = {
         messagingSenderId: "255563379338",
         appId: "1:255563379338:web:e12e5dd7741cdaa7d73373"
     },
-    ownerUid: "qmiVmnMbcqdOUDTxU8goApB5qlB2",
+    ownerUid: "BbEO7ou2wJeilZEsKEsp6YuSyeh2",
     ownerEmail: "Kasireddiyeswanth29@gmail.com",
     requireEmailVerified: false
 };
